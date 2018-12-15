0a9f8dae3722733d405385a5affa2455ea13673d
upura <upura0@gmail.com>
2018-12-15 13:58:02 +0900

    Create features

--- features/create.py
+++ features/create.py
@@ -1,188 +1,134 @@
 import pandas as pd
-import math
-import datetime
+import numpy as np
+import re as re
 
 from features.base import Feature, get_arguments, generate_features
 
 Feature.dir = 'features'
 
 
-class Datetime(Feature):
+class Pclass(Feature):
     def create_features(self):
-        self.train['datetime'] = train['datetime']
-        self.test['datetime'] = test['datetime']
+        self.train['Pclass'] = train['Pclass']
+        self.test['Pclass'] = test['Pclass']
 
 
-class Year(Feature):
+class Sex(Feature):
     def create_features(self):
-        self.train['year'] = pd.to_datetime(train['datetime']).dt.year
-        self.test['year'] = pd.to_datetime(test['datetime']).dt.year
+        self.train['Sex'] = train['Sex'].replace(['male', 'female'], [0, 1])
+        self.test['Sex'] = test['Sex'].replace(['male', 'female'], [0, 1])
 
 
-class Month(Feature):
+class FamilySize(Feature):
     def create_features(self):
-        self.train['month'] = pd.to_datetime(train['datetime']).dt.month
-        self.test['month'] = pd.to_datetime(test['datetime']).dt.month
+        self.train['FamilySize'] = train['Parch'] + train['SibSp'] + 1
+        self.test['FamilySize'] = test['Parch'] + train['SibSp'] + 1
 
 
-class Day(Feature):
+class Embarked(Feature):
     def create_features(self):
-        self.train['day'] = pd.to_datetime(train['datetime']).dt.day
-        self.test['day'] = pd.to_datetime(test['datetime']).dt.day
+        self.train['Embarked'] = train['Embarked'] \
+            .fillna(('S')) \
+            .map({'S': 0, 'C': 1, 'Q': 2}) \
+            .astype(int)
+        self.test['Embarked'] = test['Embarked'] \
+            .fillna(('S')) \
+            .map({'S': 0, 'C': 1, 'Q': 2}) \
+            .astype(int)
 
 
-class Dow(Feature):
+class Fare(Feature):
     def create_features(self):
-        self.train['dow'] = pd.to_datetime(train['datetime']).dt.weekday
-        self.test['dow'] = pd.to_datetime(test['datetime']).dt.weekday
+        data = train.append(test)
+        fare_mean = data['Fare'].mean()
+        self.train['Fare'] = pd.qcut(
+            train['Fare'].fillna(fare_mean),
+            4,
+            labels=False
+        )
+        self.test['Fare'] = pd.qcut(
+            test['Fare'].fillna(fare_mean),
+            4,
+            labels=False
+        )
 
 
-class Date_sin(Feature):
+class Age(Feature):
     def create_features(self):
-        self.train['date_sin'] = [
-            round(math.sin(math.radians(
-                ((datetime.datetime(2016, 12, 31)
-                    - datetime.datetime.strptime(
-                        date_str, '%Y-%m-%d'
-                    )).days/365)*360
-            )), 5) for date_str in train['datetime']
-        ]
-        self.test['date_sin'] = [
-            round(math.sin(math.radians(
-                ((datetime.datetime(2016, 12, 31)
-                    - datetime.datetime.strptime(
-                        date_str, '%Y-%m-%d'
-                    )).days/365)*360
-            )), 5) for date_str in test['datetime']
-        ]
+        data = train.append(test)
+        age_mean = data['Age'].mean()
+        age_std = data['Age'].std()
+        self.train['Age'] = pd.qcut(
+            train['Age'].fillna(
+                np.random.randint(age_mean - age_std, age_mean + age_std)
+            ),
+            5,
+            labels=False
+        )
+        self.test['Age'] = pd.qcut(
+            test['Age'].fillna(
+                np.random.randint(age_mean - age_std, age_mean + age_std)
+            ),
+            5,
+            labels=False
+        )
 
 
-class Date_cos(Feature):
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+
+class Title(Feature):
     def create_features(self):
-        self.train['date_cos'] = [
-            round(math.cos(math.radians((
-                    (datetime.datetime(2016, 12, 31)
-                        - datetime.datetime.strptime(
-                            date_str, '%Y-%m-%d'
-                        )).days/365
-                )*360)), 5) for date_str in train['datetime']
-            ]
-        self.test['date_cos'] = [
-            round(math.cos(math.radians((
-                    (datetime.datetime(2016, 12, 31)
-                        - datetime.datetime.strptime(
-                            date_str, '%Y-%m-%d'
-                        )).days/365
-                )*360)), 5) for date_str in test['datetime']
-            ]
+        title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
 
+        train['Title'] = train['Name'] \
+            .apply(get_title) \
+            .replace([
+                'Lady',
+                'Countess',
+                'Capt',
+                'Col',
+                'Don',
+                'Dr',
+                'Major',
+                'Rev',
+                'Sir',
+                'Jonkheer',
+                'Dona'
+            ], 'Rare') \
+            .replace(['Mlle', 'Ms', 'Mme'], ['Miss', 'Miss', 'Mrs'])
+        test['Title'] = test['Name'].map(title_mapping).fillna(0)
+        test['Title'] = test['Name'] \
+            .apply(get_title) \
+            .replace([
+                'Lady',
+                'Countess',
+                'Capt',
+                'Col',
+                'Don',
+                'Dr',
+                'Major',
+                'Rev',
+                'Sir',
+                'Jonkheer',
+                'Dona'
+            ], 'Rare') \
+            .replace(['Mlle', 'Ms', 'Mme'], ['Miss', 'Miss', 'Mrs'])
+        test['Title'] = test['Title'].map(title_mapping).fillna(0)
 
-class Month_sin(Feature):
-    def create_features(self):
-        self.train['month_sin'] = [
-            round(
-                math.sin(math.radians(m*360/12)), 5
-            ) for m in pd.to_datetime(train['datetime']).dt.month
-        ]
-        self.test['month_sin'] = [
-            round(
-                math.sin(math.radians(m*360/12)), 5
-            ) for m in pd.to_datetime(test['datetime']).dt.month
-        ]
-
-
-class Month_cos(Feature):
-    def create_features(self):
-        self.train['month_cos'] = [
-            round(
-                math.cos(math.radians(m*360/12)), 5
-            ) for m in pd.to_datetime(train['datetime']).dt.month
-        ]
-        self.test['month_cos'] = [
-            round(
-                math.cos(math.radians(m*360/12)), 5
-            ) for m in pd.to_datetime(test['datetime']).dt.month
-        ]
-
-
-class Day_sin(Feature):
-    def create_features(self):
-        self.train['day_sin'] = [
-            round(
-                math.sin(math.radians(d*360/31)), 5
-            ) for d in pd.to_datetime(train['datetime']).dt.day
-        ]
-        self.test['day_sin'] = [
-            round(
-                math.sin(math.radians(d*360/31)), 5
-            ) for d in pd.to_datetime(test['datetime']).dt.day
-        ]
-
-
-class Day_cos(Feature):
-    def create_features(self):
-        self.train['day_cos'] = [
-            round(
-                math.cos(math.radians(d*360/31)), 5
-            ) for d in pd.to_datetime(train['datetime']).dt.day
-        ]
-        self.test['day_cos'] = [
-            round(
-                math.cos(math.radians(d*360/31)), 5
-            ) for d in pd.to_datetime(test['datetime']).dt.day
-        ]
-
-
-class Dow_sin(Feature):
-    def create_features(self):
-        self.train['dow_sin'] = [
-            round(
-                math.sin(math.radians((w)*360/7)), 5
-            ) for w in pd.to_datetime(train['datetime']).dt.weekday
-        ]
-        self.test['dow_sin'] = [
-            round(
-                math.sin(math.radians((w)*360/7)), 5
-            ) for w in pd.to_datetime(test['datetime']).dt.weekday
-        ]
-
-
-class Dow_cos(Feature):
-    def create_features(self):
-        self.train['dow_cos'] = [
-            round(
-                math.cos(math.radians((w)*360/7)), 5
-            ) for w in pd.to_datetime(train['datetime']).dt.weekday
-        ]
-        self.test['dow_cos'] = [
-            round(
-                math.cos(math.radians((w)*360/7)), 5
-            ) for w in pd.to_datetime(test['datetime']).dt.weekday
-        ]
-
-
-class Holiday(Feature):
-    def create_features(self):
-        self.train['holiday'] = [
-            1 if (x == 5 or x == 6) else 0
-            for x in pd.to_datetime(train['datetime']).dt.weekday
-        ]
-        self.test['holiday'] = [
-            1 if (x == 5 or x == 6) else 0
-            for x in pd.to_datetime(test['datetime']).dt.weekday
-        ]
-
-
-class Park(Feature):
-    def create_features(self):
-        self.train['park'] = train['park']
-        self.test['park'] = test['park']
+        self.train['Title'] = train['Title']
+        self.test['Title'] = test['Title']
 
 
 if __name__ == '__main__':
     args = get_arguments()
 
-    train = pd.read_feather('./data/input/train.feather')
-    test = pd.read_feather('./data/input/test.feather')
+    train = pd.read_csv('./data/input/train.csv')
+    test = pd.read_csv('./data/input/test.csv')
 
     generate_features(globals(), args.force)
